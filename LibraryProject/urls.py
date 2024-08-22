7ac5b7cc02067bedfd9b84a563e8766b30a10b33
Ana Geguchadze <anageguchadze22@gmail.com>
2024-08-22 15:46:44 +0200

    add links

--- LibraryProject/urls.py
+++ LibraryProject/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('books/<str:title>/', views.book_detail, name='book_detail'),
-    path('add/', views.add_book),
+    path('add/', views.add_book, name='add_book'),
     path('edit/<str:title>/', views.edit_book),
     path('delete/<str:title>/', views.delete_book),
 ]
