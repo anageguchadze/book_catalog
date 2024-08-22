fac6ef37befb397ce866c666639b3af3c452fee6
Ana Geguchadze <anageguchadze22@gmail.com>
2024-08-22 15:37:15 +0200

    book catalog

---
+++ catalog/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render, redirect
+from .models import Book
+
+# Create your views here.
+def index(request):
+    books = Book.objects.all()
+    return render(request, 'index.html', {'books': books})
+
+def book_detail(request, title):
+    book = Book.objects.get(title=title)
+    return render(request, 'book_detail.html', {'book': book})
+
+def add_book(request):
+    if request.method =='POST':
+        new_book = Book(
+            title =request.POST.get('title'),
+            author =request.POST.get('author'),
+            published_year =request.POST.get('published_year'),
+            genre =request.POST.get('genre'),
+        )
+        new_book.save()
+        return redirect('index')
+    return render(request, 'add_book.html')
+
+def edit_book(request, title):
+    book = Book.objects.get(title=title)
+
+    if request.method == 'POST':
+        book.title = request.POST.get('title')
+
+        book.save()
+        return redirect('book_detail', title=book.title)
+
+    return render(request, 'edit_book.html', {'book': book})
+
+def delete_book(request, title):
+    book = Book.objects.get(title=title)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('index')
+    return render(request, 'delete_book.html', {'book':book})
